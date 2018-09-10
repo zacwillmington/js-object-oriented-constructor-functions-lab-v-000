7f51f3ced59b595949deffee56b653628d7bcf92
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 19:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function Driver (name, age, experience){
     this.age = age;
     this.experience = experience;
 };
+
+function PickupLocation (address, city){
+    this.address = address;
+    this.city = city;
+};
